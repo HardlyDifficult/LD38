252a8d01184ca18f99e4d4d7eea42b870f0119d3
Roderick Griffioen <roderick.griffioen@gmail.com>
2017-04-22 03:52:54 +0200

    Added base layout for credits screen with writing text. Drop the prefab CreditsPanel_Prefab into a canvas, you can edit the credits values and speed in the script (CreditsScreen.cs) on the root object.

---
+++ LD38/Assets/Code/Interface/CreditsScreen.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScreen : MonoBehaviour
+{
+    #region Public Data
+    public List<string> creditsMembers = new List<string>();
+
+    public GameObject creditsContentObject;
+    public GameObject creditsTextObject;
+
+    // How much time should pass between letters
+    public float timePerLetter = 0.1f;
+    // How much time the system should wait after a user
+    public float restTimePerEnd = 1.0f;
+
+    #endregion
+
+    #region Private Data
+    private string _currentText;
+    private int _textLength = 0;
+    private int _currentTextPosition = 0;
+    private float _currentTime = 0.0f;
+    private bool _atEndOfLine = false;
+
+    private string _currentCreditsMember = "";
+    private int _currentCreditsMemberIndex = 0;
+
+    private Text _currentTextBlock;
+
+    private bool _isDone = false;
+    #endregion
+
+    #region Public API
+    void Start ()
+    {
+        _currentCreditsMember = creditsMembers[_currentCreditsMemberIndex];
+        _textLength = _currentCreditsMember.Length;
+
+        GameObject tStartText = GameObject.Instantiate(creditsTextObject) as GameObject;
+        tStartText.transform.SetParent(creditsContentObject.transform);
+
+        tStartText.transform.localPosition = Vector2.zero;
+
+        Text tText = tStartText.GetComponent<Text>();
+        tText.text = "";
+
+        _currentTextBlock = tText;
+
+        RectTransform rect = creditsContentObject.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x, 90);
+    }
+
+    void Update ()
+    {
+        if (_isDone)
+            return;
+
+		if(_currentCreditsMember != " NONE")
+        {
+            if(_currentTextPosition < _textLength)
+            {
+                _currentTime += Time.deltaTime;
+
+                if(!_atEndOfLine)
+                {
+                    if(_currentTime >= timePerLetter)
+                    {
+                        char charToAdd = _currentCreditsMember[_currentTextPosition];
+
+                        _currentText += charToAdd;
+
+                        _currentTextBlock.text = _currentText;
+
+                        _currentTextPosition++;
+                        _currentTime = 0.0f;
+
+                        if(_currentTextPosition >= _textLength)
+                        {
+                            // next member
+                            _currentTextPosition = 0;
+                            _atEndOfLine = true;
+                            _currentText = "";
+                        }
+                    }
+                }
+                else
+                {
+                    if(_currentTime >= restTimePerEnd)
+                    {
+                        _currentCreditsMemberIndex++;
+
+                        if(_currentCreditsMemberIndex >= creditsMembers.Count)
+                        {
+                            _isDone = true;
+                            return;
+                        }
+
+                        //spawn new block
+                        GameObject tNewText = GameObject.Instantiate(creditsTextObject) as GameObject;
+                        tNewText.transform.SetParent(creditsContentObject.transform);
+
+                        tNewText.transform.localPosition = Vector2.zero;
+
+                        RectTransform rect = creditsContentObject.GetComponent<RectTransform>();
+                        rect.sizeDelta = new Vector2(rect.sizeDelta.x, rect.sizeDelta.y + 50); // add text size to content panel for scroll
+
+                        Text tText = tNewText.GetComponent<Text>();
+                        tText.text = "";
+
+                        _currentTextBlock = tText;
+
+                        _textLength = creditsMembers[_currentCreditsMemberIndex].Length;
+                        _currentCreditsMember = creditsMembers[_currentCreditsMemberIndex];
+
+                        _currentTextPosition = 0;
+                        _currentTime = 0.0f;
+
+                        _atEndOfLine = false;
+                    }
+                }
+            }
+        }
+	}
+    #endregion
+}
